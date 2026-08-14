8d8f10c1275b118c092df91776dbba5b763f197c
elafzhraa-cmd <elafzhraa@gmail.com>
2026-08-14 15:47:17 +0300

    Refresh RASH.IQ offline cache

--- rashiq/sw.js
+++ rashiq/sw.js
@@ -1,5 +1,5 @@
-const CACHE='rashiq-shell-v1';
-const SHELL=['./','./index.html','./style.css','./app.js','./icon.svg','./manifest.webmanifest'];
+const CACHE='rashiq-shell-v2';
+const SHELL=['./','./index.html','./style.css','./app.js','./icon.svg','./icon-192.svg','./icon-512.svg','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET')return;if(u.origin===location.origin){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))}});
